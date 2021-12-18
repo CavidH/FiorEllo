b0f3285f70a1888d8d03d13277bf020c1185ebcb
Cavid <hcavid386@gmail.com>
2021-12-18 18:18:52 +0400

    ProductImage

---
+++ FiorEllo/Models/ProductImage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FiorEllo.Models
+{
+    public class ProductImage
+    {
+        public string Id { get; set; }
+        public string Image { get; set; }
+        public bool IsMain { get; set; }
+    }
+}
