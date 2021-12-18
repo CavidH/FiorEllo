4546fdfedeeaa495fe18504c34295fc373d2d40b
Cavid <hcavid386@gmail.com>
2021-12-18 18:08:57 +0400

    18:08

--- FiorEllo/Models/Product.cs
+++ FiorEllo/Models/Product.cs
@@ -10,5 +10,7 @@ namespace FiorEllo.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public decimal Price { get; set; }
+        public int Count { get; set; }
+        public bool IsDeleted { get; set; }
     }
 }
