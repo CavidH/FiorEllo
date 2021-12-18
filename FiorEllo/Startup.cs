8b9f66e43953d7189e1359460e2586b8655d18b2
Cavid <hcavid386@gmail.com>
2021-12-18 15:43:34 +0400

    app  endpoint

--- FiorEllo/Startup.cs
+++ FiorEllo/Startup.cs
@@ -43,10 +43,7 @@ namespace FiorEllo
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
