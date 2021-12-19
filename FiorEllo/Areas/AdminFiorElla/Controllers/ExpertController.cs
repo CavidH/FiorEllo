ed1999c1cbe74547cc871c2ddfde122fdbd0f2be
Cavid <hcavid386@gmail.com>
2021-12-19 21:07:08 +0400

    Expert Done ###

---
+++ FiorEllo/Areas/AdminFiorElla/Controllers/ExpertController.cs
@@ -0,0 +1,55 @@
+﻿using FiorEllo.DAL;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FiorEllo.Areas.AdminFiorElla.Controllers
+{
+    public class ExpertController : Controller
+    {
+        // GET
+        private AppDbContext _context { get; }
+
+        public ExpertController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [Area("AdminFiorElla")]
+
+        public IActionResult Index()
+        {
+            return View(_context.Experts);
+        }
+        [Area("AdminFiorElla")]
+        public IActionResult Create()
+        {
+            return Json(new
+            {
+                Name="Create"
+            });
+        }
+        [Area("AdminFiorElla")]
+        public IActionResult Detail(int id)
+        {
+            return Json(new
+            {
+                Id=id
+            });
+        }
+        [Area("AdminFiorElla")]
+        public IActionResult Update(int id)
+        {
+            return Json(new
+            {
+                Id = id
+            });
+        }
+        [Area("AdminFiorElla")]
+        public IActionResult Delete(int id)
+        {
+            return Json(new
+            {
+                Id = id
+            });
+        }
+    }
+}
