c97e8fb0daba935b53ef2dda07cc24efd7354d56
Cavid <hcavid386@gmail.com>
2021-12-19 15:51:13 +0400

    :v::v::v:

--- FiorEllo/Areas/AdminFiorElla/Controllers/CategoryController.cs
+++ FiorEllo/Areas/AdminFiorElla/Controllers/CategoryController.cs
@@ -1,13 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using FiorEllo.DAL;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FiorEllo.Areas.AdminFiorElla.Controllers
 {
     public class CategoryController : Controller
     {
+
+        private AppDbContext _context { get; }
+
+        public CategoryController(AppDbContext context)
+        {
+            _context = context;
+        }
+
         [Area("AdminFiorElla")]
+
         public IActionResult Index()
         {
-            return View();
+            return View(_context.ProductCategories);
         }
     }
 }
