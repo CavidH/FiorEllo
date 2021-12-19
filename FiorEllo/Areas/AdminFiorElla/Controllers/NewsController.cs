91753066ce4a3fdbc58fd43d32b2279d87596214
Cavid <hcavid386@gmail.com>
2021-12-19 20:53:57 +0400

    news Done:cherries::cherries::cherries:

---
+++ FiorEllo/Areas/AdminFiorElla/Controllers/NewsController.cs
@@ -0,0 +1,55 @@
+﻿using FiorEllo.DAL;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FiorEllo.Areas.AdminFiorElla.Controllers
+{
+    public class NewsController : Controller
+    {
+        // GET
+        private AppDbContext _context { get; }
+
+        public NewsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [Area("AdminFiorElla")]
+
+        public IActionResult Index()
+        {
+            return View(_context.News);
+        }
+        [Area("AdminFiorElla")]
+        public IActionResult Create()
+        {
+            return Json(new
+            {
+                Name="Create"
+            });
+        }
+        [Area("AdminFiorElla")]
+        public IActionResult Detail(int id)
+        {
+            return Json(new
+            {
+                Id=id
+            });
+        }
+        [Area("AdminFiorElla")]
+        public IActionResult Update(int id)
+        {
+            return Json(new
+            {
+                Id = id
+            });
+        }
+        [Area("AdminFiorElla")]
+        public IActionResult Delete(int id)
+        {
+            return Json(new
+            {
+                Id = id
+            });
+        }
+    }
+}
