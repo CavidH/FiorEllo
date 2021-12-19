777c28fb32da9cb506564c484110965f4cadbf3b
Cavid <hcavid386@gmail.com>
2021-12-19 19:24:13 +0400

    :speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon::speech_balloon:

--- FiorEllo/Areas/AdminFiorElla/Controllers/SliderController.cs
+++ FiorEllo/Areas/AdminFiorElla/Controllers/SliderController.cs
@@ -1,13 +1,54 @@
-﻿using Microsoft.AspNetCore.Mvc; 
+﻿using FiorEllo.DAL;
+using Microsoft.AspNetCore.Mvc; 
 
 namespace FiorEllo.Areas.AdminFiorElla.Controllers
 {
     public class SliderController : Controller
     {
+        private AppDbContext _context { get; }
+
+        public SliderController(AppDbContext context)
+        {
+            _context = context;
+        }
         [Area("AdminFiorElla")]
         public IActionResult Index()
         {
-            return View();
+            
+
+            return View(_context.Sliders);
+        }
+        [Area("AdminFiorElla")]
+        public IActionResult Create()
+        {
+            return Json(new
+            {
+                Name = "Create"
+            });
+        }
+        [Area("AdminFiorElla")]
+        public IActionResult Detail(int id)
+        {
+            return Json(new
+            {
+                Id = id
+            });
+        }
+        [Area("AdminFiorElla")]
+        public IActionResult Update(int id)
+        {
+            return Json(new
+            {
+                Id = id
+            });
+        }
+        [Area("AdminFiorElla")]
+        public IActionResult Delete(int id)
+        {
+            return Json(new
+            {
+                Id = id
+            });
         }
     }
 }
