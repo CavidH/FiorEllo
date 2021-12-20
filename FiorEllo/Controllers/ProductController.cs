604d7eb3b46267f3108a78ee65b348925cc09190
tu201906038 <tu201906038@215-7>
2021-12-20 19:07:41 +0400

    ClassRoom

---
+++ FiorEllo/Controllers/ProductController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FiorEllo.DAL;
+using Microsoft.EntityFrameworkCore;
+
+namespace FiorEllo.ViewModel
+{
+    public class ProductController : Controller
+    {
+        public AppDbContext _context { get; }
+
+        public ProductController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var Products = await _context
+                .Products
+                .Where(product => product.IsDeleted == false)
+                .Include(product => product.Image)
+                .OrderByDescending(product => product.Id)
+                .Take(8)
+                .ToListAsync();
+            return View(Products);
+        }
+
+        public async Task<IActionResult> LoadProduct()
+        {
+            var model = await _context
+                .Products
+                .OrderByDescending(product => product.Id)
+                .Skip(8)
+                .Take(8)
+                .Where(product => product.IsDeleted == false)
+                .Include(product => product.Image)
+                .ToListAsync();
+
+            return PartialView("_ProductPartial", model);
+        }
+    }
+}
