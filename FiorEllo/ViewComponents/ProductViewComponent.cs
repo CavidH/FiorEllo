f5aa05228751f714796c699b0e8b8733501056c6
Cavid <hcavid386@gmail.com>
2021-12-23 00:36:51 +0400

    :palm_tree::palm_tree::p:palm_tree:alm_tree::palm_tree::palm_tree::palm_tree:

--- FiorEllo/ViewComponents/ProductViewComponent.cs
+++ FiorEllo/ViewComponents/ProductViewComponent.cs
@@ -1,15 +1,31 @@
-﻿using FiorEllo.DAL;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using FiorEllo.DAL;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace FiorEllo.ViewComponents
 {
-    public class ProductViewComponent :ViewComponent
+    public class ProductViewComponent : ViewComponent
     {
         private AppDbContext _context;
+
         public ProductViewComponent(AppDbContext context)
         {
             _context = context;
         }
-        
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var Model = await _context
+                .Products
+                .Where(product => product.IsDeleted == false)
+                .Include(product => product.Category)
+                .Include(product => product.Image)
+                .OrderByDescending(product => product.Id) //en yeni  productlari goturmek uchun. sondan evvele  
+                .Take(8) //artiq data gelisnin qabaqini aliriq
+                .ToListAsync();
+            return View(await Task.FromResult(Model));
+        }
     }
 }
