3ce21cdaa62f18a5bf2c1b148a9bed89901bc07b
Cavid <hcavid386@gmail.com>
2021-12-24 12:09:54 +0400

    basket Counter Done ^^^

--- FiorEllo/ViewComponents/HeaderViewComponent.cs
+++ FiorEllo/ViewComponents/HeaderViewComponent.cs
@@ -1,18 +1,37 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using FiorEllo.DAL;
+using FiorEllo.ViewModel;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 
 namespace FiorEllo.ViewComponents
 {
     public class HeaderViewComponent : ViewComponent
     {
+        public List<BasketVM> basket;
          
         public async Task<IViewComponentResult> InvokeAsync()
         {
-             
+            
+            ViewBag.count =GetBasket().Count;
             return View();
+        }
+        private List<BasketVM> GetBasket()
+        {
+            
+            if (Request.Cookies["basket"]!=null)
+            {
+                basket = JsonConvert.DeserializeObject<List<BasketVM>>(Request.Cookies["basket"]);
+            }
+            else
+            {
+                basket = new List<BasketVM>();
+            }
+
+            return basket;
         }
     }
 }
