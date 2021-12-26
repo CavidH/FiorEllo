e408e03793031b7db10bfaf3ef33f8561405134b
Cavid Haciyev <cavid.haciyev.2002@mail.ru>
2021-12-26 23:31:36 +0400

    About Done ^^^

--- FiorEllo/Areas/AdminFiorElla/Controllers/AboutController.cs
+++ FiorEllo/Areas/AdminFiorElla/Controllers/AboutController.cs
@@ -1,11 +1,14 @@
-﻿using FiorEllo.DAL;
+﻿using System;
+using System.Threading.Tasks;
+using FiorEllo.DAL;
+using FiorEllo.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FiorEllo.Areas.AdminFiorElla.Controllers
 {
+    [Area("AdminFiorElla")]
     public class AboutController : Controller
     {
-
         private AppDbContext _context { get; }
 
         public AboutController(AppDbContext context)
@@ -13,47 +16,64 @@ namespace FiorEllo.Areas.AdminFiorElla.Controllers
             _context = context;
         }
 
-        [Area("AdminFiorElla")]
-
         public IActionResult Index()
         {
             return View(_context.Abouts);
         }
 
-        [Area("AdminFiorElla")]
         public IActionResult Create()
         {
-            return Json(new
-            {
-                Name = "Create"
-            });
+            return View();
         }
 
-        [Area("AdminFiorElla")]
-        public IActionResult Detail(int id)
+        [HttpPost]
+        public async Task<IActionResult> Create(About about)
         {
-            return Json(new
-            {
-                Id = id
-            });
+            about.CreaDate = DateTime.Now;
+            _context.Abouts.Add(about);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
         }
 
-        [Area("AdminFiorElla")]
-        public IActionResult Update(int id)
+        // public IActionResult Detail(int id)
+        // {
+        //     return Json(new
+        //     {
+        //         Id = id
+        //     });
+        // }
+
+        public async Task<IActionResult> Update(int id)
         {
-            return Json(new
-            {
-                Id = id
-            });
+            var About = await GetAboutById(id);
+            return View(About);
         }
 
-        [Area("AdminFiorElla")]
-        public IActionResult Delete(int id)
+        [HttpPost]
+        public async Task<IActionResult> Update(int id, About about)
         {
-            return Json(new
-            {
-                Id = id
-            });
+            var About = await GetAboutById(id);
+            About.Title = about.Title;
+            About.Head = about.Head;
+            About.Image = about.Image;
+            About.CreaDate=DateTime.Now;
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var about = await GetAboutById(id);
+            _context.Abouts.Remove(about);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private async Task<About> GetAboutById(int id)
+        {
+            var About = await _context.Abouts.FindAsync(id);
+            return About;
         }
     }
 }
