ec4e1c1903b58a6c39c2f2c0612073e19138fed9
Cavid Haciyev <cavid.haciyev.2002@mail.ru>
2021-12-26 13:17:32 +0400

    :rose:

--- FiorEllo/ViewComponents/FooterViewComponent.cs
+++ FiorEllo/ViewComponents/FooterViewComponent.cs
@@ -1,8 +1,5 @@
-﻿using System.Linq;
-using System.Threading.Tasks;
-using FiorEllo.DAL;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace FiorEllo.ViewComponents
 {
