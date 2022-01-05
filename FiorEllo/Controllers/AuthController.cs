86f6729c2af15069b7e3055bd755403d4b3597d8
Cavid Haciyev <cavid.haciyev.2002@mail.ru>
2022-01-05 19:45:50 +0400

    Auth msg

--- FiorEllo/Controllers/AuthController.cs
+++ FiorEllo/Controllers/AuthController.cs
@@ -3,13 +3,48 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FiorEllo.Models;
+using FiorEllo.ViewModel.Auth;
+using Microsoft.AspNetCore.Identity;
 
 namespace FiorEllo.Controllers
 {
     public class AuthController : Controller
     {
+        private UserManager<ApplicationUser> _userManager;
+
+        public AuthController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         public IActionResult Register()
         {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterVM register)
+        {
+            if (ModelState.IsValid) return View(register);
+            ApplicationUser newUser = new ApplicationUser
+            {
+                FullName = register.FullName,
+                UserName = register.UserName,
+                Email = register.Email
+            };
+            var IdentityResult = await _userManager.CreateAsync(newUser, register.Password);
+            if (!IdentityResult.Succeeded)
+            {
+                foreach (var error in IdentityResult.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+                return View(register);
+            }
+
             return View();
         }
     }
