b4b79ea4f0d6016ae30bfde1ffab6bece53cedfc
Cavid Haciyev <cavid.haciyev.2002@mail.ru>
2022-01-06 23:45:00 +0400

    Login VM done

---
+++ FiorEllo/ViewModel/Auth/LoginVM.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FiorEllo.ViewModel.Auth
+{
+    public class LoginVM
+    {
+        [Required, MaxLength(255),DataType(DataType.EmailAddress)]
+        public string EMail { get; set; }
+        [Required, MaxLength(255), DataType(DataType.Password)]
+        public string Passvord { get; set; }
+    }
+}
